e0d1f23d3c2f067933b25e4b38fb1d8687e43b17
jeonghoonji <wlwjdgns0920@naver.com>
2022-11-26 02:58:06 +0900

    feet: 챕터2 7단계

---
+++ Landmarks_Transform/Landmarks_Transform/Landmark.swift
@@ -0,0 +1,41 @@
+//
+//  Landmark.swift
+//  Landmarks_Transform
+//
+//  Created by 지정훈 on 2022/11/26.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark : Hashable, Codable{
+    var name : String
+    var category : String
+    var city : String
+    var state : String
+    var id : Int
+    var isFeatured : Bool
+    var isFavorite : Bool
+    var park : String
+    
+    private var imageName : String
+    
+    var image : Image{
+        Image(imageName)
+    }
+    
+    private var coordinates : Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+            CLLocationCoordinate2D(
+                latitude: coordinates.latitude,
+                longitude: coordinates.longitude)
+        }
+    
+    struct Coordinates : Hashable, Codable{
+        var longitude : Double
+        var latitude : Double
+    }
+}
+ 
